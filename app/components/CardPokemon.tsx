7fe83962d7747ed1223817839d46a69e654e6251
OhMyLaia <ohmylaia@live.com>
2025-10-05 11:11:12 +0200

    added another conditional to colour picking by type

--- app/components/CardPokemon.tsx
+++ app/components/CardPokemon.tsx
@@ -40,13 +40,16 @@ function CardPokemon({ pokemon, number }: { pokemon: Pokemon, number: number }) 
     const getPredominantType = (types?: { type: { name: string } }[]) => {
         if (!types || types.length === 0) {
             return "rgb(255, 255, 255)";
-        } else {
-            return types[0].type.name
         }
+        // else {
+        //     return types[0].type.name
+        // }
 
-        // return types.length > 1
-        //     ? types[1].type.name
-        //     : types[0].type.name;
+        return types.length > 1
+            ? types[1].type.name === "normal"
+                ? types[0].type.name
+                : types[1].type.name
+                : types[0].type.name;
     }
 
     const predominantType: string = getPredominantType(data?.types ?? (pokemon as any).types);
